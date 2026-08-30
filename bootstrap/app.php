138faaa1ac97e23cb009d1d97c05d6952444eff1
Rsplist0522 <edmundtehwh@gmail.com>
2026-08-30 23:01:50 +0800

    force HTTPS scheme

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
         $middleware->web(append: [SetLocale::class]);
 
         $middleware->redirectGuestsTo(fn () => route('login'));
